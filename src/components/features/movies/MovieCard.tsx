daa7f296d9c4b8086c128f56c100ace4cb56f08d
Smyku6 <smyku6@gmail.com>
2025-09-14 11:40:32 +0200

    files moved to another folder

--- src/components/features/movies/MovieCard.tsx
+++ src/components/features/movies/MovieCard.tsx
@@ -14,14 +14,6 @@ export function MovieCard({ movie }: { movie: MovieOutput }) {
             className="group rounded-xl border border-gray-200 bg-white p-4 shadow-sm hover:shadow-md transition"
             title={originalTitle}
         >
-            {/* Placeholder na plakat (opcjonalnie w przyszłości podłączysz src) */}
-            {/*<div className="aspect-[2/3] w-full rounded-lg bg-gray-100 mb-3 overflow-hidden">*/}
-            {/*    /!* <img src={movie.poster ?? "/placeholder.svg"} alt={title} className="h-full w-full object-cover" /> *!/*/}
-            {/*    <div className="h-full w-full flex items-center justify-center text-gray-400 text-sm">*/}
-            {/*        Poster*/}
-            {/*    </div>*/}
-            {/*</div>*/}
-
             <h3 className="line-clamp-2 font-medium text-gray-900">{originalTitle}</h3>
 
             <p className="mt-1 text-xs text-gray-600 tabular-nums">
